f19772831aaf9f3e67de7246f893641b3bcb2b9a
Mangogogo-Zeng <seonphe@126.com>
2025-03-01 20:57:35 +0800

    1st submit

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+def sum_two_numbers(a, b):
+    return a + b
+
+# Example usage
+num1 = 5
+num2 = 3
+result = sum_two_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is {result}")
+print("helloworld")
+product = num1 * num2
+print(f"The product of {num1} and {num2} is {product}")
